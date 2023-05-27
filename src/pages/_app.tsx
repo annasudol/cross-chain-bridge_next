caf1dc32739899e832263dc049ea803d4f649e3b
Anna Sudol <sudolann@gmail.com>
2023-05-27 17:18:00 +0100

    fix(FE): styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,14 @@ import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
-// import '@/styles/colors.css';
-// import CustomWeb3Provider from '@/components/CustomWeb3Provider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-
       <ThirdwebProvider  activeChain="goerli">
-      {/* <CustomWeb3Provider> */}
-            <ChakraProvider>
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Component {...pageProps} />
         </ChakraProvider>
-      {/* </CustomWeb3Provider> */}
       </ThirdwebProvider>
-
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,24 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
-import { useAddress } from "@thirdweb-dev/react";
 import * as React from 'react';
 
-// import ConnectWallet from '@/components/connectWallet';
 import Seo from '@/components/Seo';
 import { Tabs } from "@/components/tabs";
 
 import { AppConfig } from '@/utils/AppConfig';
 
 export default function HomePage() {
-    const address = useAddress();
-
   return (
-    <div className='home'>
+    <div className='w-screen h-screen overflow-hidden grad'>
       <Seo />
       <main>
-        {address}
-        <section className='max-w-lg mx-auto p-4'>
+        <section className='max-w-lg mx-auto p-4 z-10 relative'>
           <ConnectWallet />
           <div className='flex justify-center'>
             <Tabs />
           </div>
         </section>
       </main>
-      <footer className='fixed inset-x-auto bottom-2 w-full py-4 text-sm text-white flex justify-center'>
+      <footer className='z-0 inset-x-auto text-sm text-white flex justify-center'>
         <div className='max-w-xl text-center'>
           <p>
             © Copyright {new Date().getFullYear()} {AppConfig.title}
