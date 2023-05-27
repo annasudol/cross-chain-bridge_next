525a9df1e5fc8b6439d12f57d2e266aa87ca58f2
Anna Sudol <sudolann@gmail.com>
2023-05-27 17:53:23 +0100

    chore(FE): added EtherScanMessage

---
+++ src/components/EtherscanMessage/index.tsx
@@ -0,0 +1,28 @@
+import { Link, Text } from '@chakra-ui/react'
+import { useChainId } from "@thirdweb-dev/react";
+
+import { etherscan_address } from '@/utils/contrants';
+
+
+interface EtherScanMessageProps {
+  title: string
+  blockHash: string
+}
+export const EtherScanMessage: React.FC<EtherScanMessageProps> = ({
+  title,
+  blockHash,
+}) => {
+  const chain = useChainId();
+
+
+  return (
+    <>
+      <Text>{title}</Text>
+      <Text>
+        <Link href={etherscan_address(chain || 5, blockHash)} isExternal>
+          View on Etherscan
+        </Link>
+      </Text>
+    </>
+  )
+}
