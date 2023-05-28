7b60744422bd8de11c137908e9eca9869343830c
Anna Sudol <sudolann@gmail.com>
2023-05-28 09:11:23 +0100

    fix(FE): ThirdwebProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { BinanceTestnet, Goerli } from '@thirdweb-dev/chains';
 import { ThirdwebProvider } from '@thirdweb-dev/react';
 import { AppProps } from 'next/app';
 
@@ -6,7 +7,9 @@ import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain='goerli'>
+    <ThirdwebProvider
+      supportedChains={[Goerli, BinanceTestnet]}
+    >
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
